31a8217ebb2c986d1b2f9ff11c8a3c5ebbdbdbdb
Liam Wise <liamwise@Liams-MacBook-Air.local>
2025-11-12 09:46:40 +0900

    reorganize modules

--- simplesynth/main.go
+++ simplesynth/main.go
@@ -6,10 +6,10 @@ import (
 	"os"
 	"time"
 
-	"github.com/lbwise/audiowrld/simplesynth/effects"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
+	"github.com/lbwise/audiowrld/effects"
 
 	"github.com/lbwise/audiowrld/simplesynth/io"
 	"github.com/lbwise/audiowrld/simplesynth/notes"
